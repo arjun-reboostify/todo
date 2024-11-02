4271ac2aaf2c5aeb29981d9cb89bfa8e6ef3adf8
arjun-reboostify <reboostify@gmail.com>
2024-11-02 20:37:08 +0530

    d

--- src/services/firebaseConfig.ts
+++ src/services/firebaseConfig.ts
@@ -1,12 +1,12 @@
 const config = {
-  apiKey: "AIzaSyDBph54Brl4-HJTRW5S0vJlZMM-nc-nkdg",
-  authDomain: "reboostify-f18db.firebaseapp.com",
-  databaseURL: "https://reboostify-f18db-default-rtdb.firebaseio.com",
-  projectId: "reboostify-f18db",
-  storageBucket: "reboostify-f18db.appspot.com",
-  messagingSenderId: "870800978453",
-  appId: "1:870800978453:web:cffcfca384b9d1de3f141c",
-  measurementId: "G-Z86QKJ0ND8"
+  apiKey: 'AIzaSyDBph54Brl4-HJTRW5S0vJlZMM-nc-nkdg',
+authDomain: 'reboostify-f18db.firebaseapp.com',
+databaseURL: 'https://reboostify-f18db-default-rtdb.firebaseio.com',
+projectId: 'reboostify-f18db',
+storageBucket: 'reboostify-f18db.appspot.com',
+messagingSenderId: '870800978453',
+appId: '1:870800978453:web:cffcfca384b9d1de3f141c',
+measurementId: 'G-Z86QKJ0ND8'
 };
 
 type IConfig = typeof config;
